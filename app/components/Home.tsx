ab80f9aa9ddc3f3ea7f5a032895d56f1ca3e224c
Don-Debug <elision.q23@gmail.com>
2023-10-01 21:16:21 +0200

    navbar -v 0.1 done

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import Sector from "./Sector";
 
 export default function Hero() {
   return (
     <div>
       <Navbar />
+      <Sector />
     </div>
   );
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,59 +1,42 @@
 "use client";
 
-import { Box, Flex } from "@chakra-ui/react";
+import { MdLocalGroceryStore } from "react-icons/md";
+
+import {
+  Avatar,
+  AvatarBadge,
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Badge,
+} from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
     <Flex
-      justifyContent="space-around"
+      justifyContent="space-between"
       p="2rem"
       background="#2f2f2f"
       color="white"
+      alignContent="center"
     >
-      <Box
-        py="1rem"
-        px="3rem"
-        cursor="pointer"
-        borderBottom="2px"
-        borderColor="white"
-        display="flex"
-        justifyContent="center"
-      >
-        Instagram
-      </Box>
-      <Box
-        py="1rem"
-        px="3rem"
-        cursor="pointer"
-        borderBottom="2px"
-        borderColor="white"
-        display="flex"
-        justifyContent="center"
-      >
-        Tik Tok
-      </Box>
-      <Box
-        py="1rem"
-        px="3rem"
-        cursor="pointer"
-        borderBottom="2px"
-        borderColor="white"
-        display="flex"
-        justifyContent="center"
-      >
-        You Tube
-      </Box>
-      <Box
-        py="1rem"
-        px="3rem"
-        cursor="pointer"
-        borderBottom="2px"
-        borderColor="white"
-        display="flex"
-        justifyContent="center"
-      >
-        X
-      </Box>
+      <Flex w="50%" alignItems="center" justifyContent="flex-start">
+        <Heading>NO NAME</Heading>
+      </Flex>
+      <Flex justifyContent="center" w="40%" alignItems="center">
+        <Box px={10} cursor="pointer">
+          <MdLocalGroceryStore size={35} />
+        </Box>
+        <Badge ml="1" fontSize="1rem" colorScheme="green" alignSelf="start">
+          49$
+        </Badge>
+        <Stack direction="row" spacing={4}>
+          <Avatar>
+            <AvatarBadge boxSize="1.25em" bg="green.500" />
+          </Avatar>
+        </Stack>
+      </Flex>
     </Flex>
   );
 }
